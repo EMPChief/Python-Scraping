12d75c78e3bcaffe523e5b43796617ae86bd19d0
empchief <bjornmagnekristensen1999@gmail.com>
2023-12-28 17:12:11 +0100

    Add retry mechanism for HTTP requests in HackerNewsScraper.py

--- Scrapers/HackerNewsScraper.py
+++ Scrapers/HackerNewsScraper.py
@@ -2,27 +2,52 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 import pprint as pp
 import time
+# https://scrapy.org/
 
 
 def sort_hackernews_list(hm_list):
     return sorted(hm_list, key=lambda k: k['points'], reverse=True)
 
 
+def request_with_retry(url, max_retries=3, delay=5):
+    for attempt in range(max_retries):
+        try:
+            response = rq.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+            response.raise_for_status()
+            return response
+        except rq.exceptions.HTTPError as e:
+            print(f"HTTP error on attempt {attempt+1}: {e}")
+        except rq.exceptions.RequestException as e:
+            print(f"Request error on attempt {attempt+1}: {e}")
+        time.sleep(delay)
+    return None
+
+
 def scrape_hn_page(url):
-    res = rq.get(url)
+    res = request_with_retry(url)
+    if not res:
+        return []
+
     soup = bs(res.text, 'html.parser')
     links = soup.select('.titleline')
     votes = soup.select('.score')
 
     hn_list = []
     for idx, link in enumerate(links):
-        title = link.get_text()
-        href = link.get('href', None)
-        points_text = votes[idx].get_text() if idx < len(votes) else '0 points'
-        points = int(points_text.replace(' points', ''))
+        try:
+            title = link.get_text()
+            href = link.get('href', None)
+            points_text = votes[idx].get_text(
+            ) if idx < len(votes) else '0 points'
+            points = int(points_text.replace(' points', ''))
 
-        if points > 100:
-            hn_list.append({'title': title, 'href': href, 'points': points})
+            if points > 100:
+                hn_list.append(
+                    {'title': title, 'href': href, 'points': points})
+        except IndexError:
+            print(f"Error parsing item at index {idx}")
+        except ValueError:
+            print(f"Error parsing points for item at index {idx}")
 
     return hn_list
 
@@ -32,11 +57,15 @@ def create_custom_news_df(pages=1):
     hn_full_list = []
     url = base_url
 
-    for _ in range(pages):
+    for page_num in range(pages):
+        print(f"\nScraping page {page_num + 1}...")
         hn_list = scrape_hn_page(url)
         hn_full_list.extend(hn_list)
 
-        res = rq.get(url)
+        res = request_with_retry(url)
+        if not res:
+            break
+
         soup = bs(res.text, 'html.parser')
         more_link = soup.select('.morelink')
         if more_link:
@@ -45,12 +74,16 @@ def create_custom_news_df(pages=1):
                 more_href = base_url + more_href
             url = more_href
         else:
+            print("No more pages found.")
             break
 
-        if _ < pages - 1:
+        if page_num < pages - 1:
+            print("Waiting for 30 seconds before next request...")
             time.sleep(30)
 
-    return sort_hackernews_list(hn_full_list)
+    return hn_full_list
 
 
-pp.pprint(create_custom_news_df(5))
+hn_data = create_custom_news_df(2)
+pprinter = pp.PrettyPrinter(indent=4)
+pprinter.pprint(hn_data)
