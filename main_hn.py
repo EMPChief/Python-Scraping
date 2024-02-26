70c9d705d4cb3f1f97f9114fbb9ff108fa8d2e30
EMPChief <bjornmagnekristensen1999@gmail.com>
2024-02-26 17:53:09 +0100

    Refactor code to insert data in batches

--- main_hn.py
+++ main_hn.py
@@ -3,30 +3,48 @@ from dbs import My_Database
 import pandas as pd
 import time
 
+
+TABLE_NAME = 'hackernews'
+DATABASE_NAME = 'database.db'
+PAGE_NUMBER = 40
+
+
+def clean_title(title):
+    return title.strip().lower()
+
+
+def insert_batch_data(db, data, columns):
+    batch_size = 5
+    for i in range(0, len(data), batch_size):
+        batch = data.iloc[i:i+batch_size]
+        db.insert_many_rows(batch.values.tolist(), columns[1:])
+        print(f"Batch {i // batch_size + 1} inserted into database.")
+
+
 if __name__ == '__main__':
-    columns = ['id INTEGER PRIMARY KEY', 'title TEXT', 'href TEXT', 'points INTEGER']
+    columns = ['id INTEGER PRIMARY KEY',
+               'title TEXT', 'href TEXT', 'points INTEGER']
 
     try:
         hacker_news = HackerNewsScraper()
-        hn_data = hacker_news.create_custom_news_df(10)
+        hn_data = hacker_news.create_custom_news_df(PAGE_NUMBER)
 
-        db = My_Database(db_name='database.db', tb='hackernews')
+        db = My_Database(db_name=DATABASE_NAME, tb=TABLE_NAME)
         db.create_table(columns)
 
         existing_data = db.select_all_data()
 
         if not existing_data or len(existing_data) == 0:
-            existing_band_city_date = set()
+            existing_titles = set()
         else:
-            existing_band_city_date = set(
-                map(lambda x: (x[1], x[2], x[3]), existing_data))
+            existing_titles = set(
+                map(lambda x: clean_title(x[1]), existing_data))
 
-        new_data = hn_data[~hn_data.apply(lambda x: (
-            x['title'], x['href'], x['points']), axis=1).isin(existing_band_city_date)]
+        new_data = hn_data[~hn_data['title'].apply(
+            clean_title).isin(existing_titles)]
 
         if not new_data.empty:
-            new_data = new_data[['title', 'href', 'points']]
-            db.insert_many_rows(new_data.values.tolist(), columns[1:])
+            insert_batch_data(db, new_data, columns)
             print("New data inserted into database.")
         else:
             print("No new data to insert.")
